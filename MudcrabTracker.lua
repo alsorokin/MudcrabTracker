6cd05864d853291538ce261bb4a2905cd54aa526
Aleksandr Sorokin <me@snay.me>
2025-01-29 02:46:36 +0400

    Enhance mudcrab tracking: add support for player pets and companions, update kill counter message, and implement slash command for total kills.

--- MudcrabTracker.lua
+++ MudcrabTracker.lua
@@ -27,15 +27,24 @@ end
 local function onCombatEvent(_eventCode_, result, _isError_, _abilityName_, _abilityGraphic_, _abilityActionSlotType_, _sourceName_,
         _sourceType_, targetName, _targetType_, _hitValue_, _powerType_, _damageType_, _log_, _sourceUnitId_, _targetUnitId_,
         _abilityId_, _overflow_)
-    if result == ACTION_RESULT_DIED_XP or result == ACTION_RESULT_DIED then
-        if targetName == "Mudcrab" then
-            Print(targetName .. " died")
+    if (result == ACTION_RESULT_DIED_XP) or (result == ACTION_RESULT_DIED) then
+        -- Print("Killed a " .. targetName)
+        if (string.find(targetName, "Mudcrab") ~= nil) or (string.find(targetName, "Coral Crab") ~= nil) then
             db.counter = db.counter + 1
+            Print("You killed a mudcrab! Total crabs killed: " .. db.counter)
             UpdateLabel()
         end
     end
 end
 
 EVENT_MANAGER:RegisterForEvent(ADDON_NAME .. "_start", EVENT_ADD_ON_LOADED, onAddOnLoaded)
-EVENT_MANAGER:RegisterForEvent(ADDON_NAME .. "_combat_player", EVENT_COMBAT_EVENT, onCombatEvent)
-EVENT_MANAGER:AddFilterForEvent(ADDON_NAME .. "_combat_player", EVENT_COMBAT_EVENT, REGISTER_FILTER_SOURCE_COMBAT_UNIT_TYPE, COMBAT_UNIT_TYPE_PLAYER)
+-- EVENT_MANAGER:RegisterForEvent(ADDON_NAME .. "_combat_player", EVENT_COMBAT_EVENT, onCombatEvent)
+-- EVENT_MANAGER:AddFilterForEvent(ADDON_NAME .. "_combat_player", EVENT_COMBAT_EVENT, REGISTER_FILTER_SOURCE_COMBAT_UNIT_TYPE, COMBAT_UNIT_TYPE_PLAYER)
+EVENT_MANAGER:RegisterForEvent(ADDON_NAME .. "_combat_player_pet", EVENT_COMBAT_EVENT, onCombatEvent)
+EVENT_MANAGER:AddFilterForEvent(ADDON_NAME .. "_combat_player_pet", EVENT_COMBAT_EVENT, REGISTER_FILTER_SOURCE_COMBAT_UNIT_TYPE, COMBAT_UNIT_TYPE_PLAYER_PET)
+EVENT_MANAGER:RegisterForEvent(ADDON_NAME .. "_combat_companion", EVENT_COMBAT_EVENT, onCombatEvent)
+EVENT_MANAGER:AddFilterForEvent(ADDON_NAME .. "_combat_companion", EVENT_COMBAT_EVENT, REGISTER_FILTER_SOURCE_COMBAT_UNIT_TYPE, COMBAT_UNIT_TYPE_COMPANION)
+
+SLASH_COMMANDS["/crabs"] = function()
+    Print("Mudcrabs exterminated: " .. db.counter)
+end
