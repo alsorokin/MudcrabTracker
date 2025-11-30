51af5a0ae699530444afbbe28cca78dc6023e73a
Aleksandr Sorokin <me@snay.me>
2025-11-30 13:44:55 +0400

    add Linux install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+cp ./MudcrabTracker.lua -t ~/.local/share/Steam/steamapps/compatdata/306130/pfx/drive_c/users/steamuser/Documents/Elder\ Scrolls\ Online/live/AddOns/MudcrabTracker
+cp ./MudcrabTracker.txt -t ~/.local/share/Steam/steamapps/compatdata/306130/pfx/drive_c/users/steamuser/Documents/Elder\ Scrolls\ Online/live/AddOns/MudcrabTracker
+cp ./MudcrabTracker.xml -t ~/.local/share/Steam/steamapps/compatdata/306130/pfx/drive_c/users/steamuser/Documents/Elder\ Scrolls\ Online/live/AddOns/MudcrabTracker
+
