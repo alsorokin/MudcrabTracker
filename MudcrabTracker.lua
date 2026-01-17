2e66057d03faa5316e7efb6cf0cd9b10dbe6ce8c
Aleksandr Sorokin <me@snay.me>
2026-01-18 00:21:50 +0400

    Add crabtitles

--- MudcrabTracker.lua
+++ MudcrabTracker.lua
@@ -7,36 +7,89 @@ local DEFAULT_CRABS_LABEL_TOP = 30
 local cumulativeScore = 0
 local cumulativeCrabs = 0
 
+local playerTitles = {
+	["en"] = {
+		{ threshold = 0, title = "Mudcrab Enthusiast" },
+		{ threshold = 10, title = "Mudcrab Wrangler" },
+		{ threshold = 100, title = "Mudcrab Hunter" },
+		{ threshold = 250, title = "Mudcrab Exterminator" },
+		{ threshold = 500, title = "Mudcrab Slayer" },
+		{ threshold = 1000, title = "Mudcrab Vanquisher" },
+		{ threshold = 2500, title = "Mudcrab Conqueror" },
+		{ threshold = 5000, title = "Mudcrab Champion" },
+		{ threshold = 10000, title = "Mudcrab Legend" },
+		{ threshold = 100000, title = "Mudcrab Lord" },
+		{ threshold = 1000000, title = "Mudcrab Deity" },
+	},
+	["de"] = {
+		{ threshold = 0, title = "Schlammkrabben-Enthusiast" },
+		{ threshold = 10, title = "Schlammkrabben-Hüter" },
+		{ threshold = 100, title = "Schlammkrabben-Jäger" },
+		{ threshold = 250, title = "Schlammkrabben-Ausrotter" },
+		{ threshold = 500, title = "Schlammkrabben-Schlächter" },
+		{ threshold = 1000, title = "Schlammkrabben-Besieger" },
+		{ threshold = 2500, title = "Schlammkrabben-Eroberer" },
+		{ threshold = 5000, title = "Schlammkrabben-Champion" },
+		{ threshold = 10000, title = "Schlammkrabben-Legende" },
+		{ threshold = 100000, title = "Schlammkrabben-Lord" },
+		{ threshold = 1000000, title = "Schlammkrabben-Gottheit" },
+	},
+	["ru"] = {
+		{ threshold = 0, title = "Любитель Грязекрабов" },
+		{ threshold = 10, title = "Укротитель Грязекрабов" },
+		{ threshold = 100, title = "Охотник на Грязекрабов" },
+		{ threshold = 250, title = "Истребитель Грязекрабов" },
+		{ threshold = 500, title = "Убийца Грязекрабов" },
+		{ threshold = 1000, title = "Победитель Грязекрабов" },
+		{ threshold = 2500, title = "Завоеватель Грязекрабов" },
+		{ threshold = 5000, title = "Чемпион Грязекрабов" },
+		{ threshold = 10000, title = "Легенда Грязекрабов" },
+		{ threshold = 100000, title = "Властелин Грязекрабов" },
+		{ threshold = 1000000, title = "Грязекрабий Бог" },
+	},
+}
+local titlesOfClientLang = playerTitles[clientLang]
+local lastPlayerTitle = ""
+
 local localizedStrings = {
 	["en"] = {
 		MUDCRAB_TRACKER_LABEL = "Crab score: %s",
-		CHAT_MESSAGE_MUDCRAB_KILLED = "You killed %s. Total crab score: %s",
-		CHAT_MESSAGE_BOSS_KILLED = "At last. The shores are free from this terror you call... %s. You earned %s points! Total crab score: %s",
+		MUDCRAB_TRACKER_TITLE = "Title: %s",
+		CHAT_MESSAGE_MUDCRAB_KILLED = "You killed |cAAAAFF%s|r. Total crab score: |cAAAAFF%s|r",
+		CHAT_MESSAGE_BOSS_KILLED = "At last. The shores are free from this terror you call... |cAAAAFF%s|r. You earned |cAAAAFF%s|r points! Total crab score: |cAAAAFF%s|r",
 		CHAT_MESSAGE_TRACKER_TOGGLE_ON = "Mudcrab Tracker is now visible.",
 		CHAT_MESSAGE_TRACKER_TOGGLE_OFF = "Mudcrab Tracker is now hidden.",
-		CHAT_MESSAGE_CRABSTAT = "Crab score: %s",
-		CHAT_MESSAGE_MULTIPLE_CRABS_KILLED = "You killed %s crabs, good job! Total crab score: %s",
-		CHAT_MESSAGE_CRABS_TO_CHAT = "[MudcrabTracker] My crab score is: %s",
+		CHAT_MESSAGE_CRABSTAT = "Crab score: |cAAAAFF%s|r. You are a |cAAAAFF%s|r!",
+		CHAT_MESSAGE_MULTIPLE_CRABS_KILLED = "You killed |cAAAAFF%s|r crabs, good job! Total crab score: |cAAAAFF%s|r",
+		CHAT_MESSAGE_CRABS_TO_CHAT = "[MudcrabTracker] My crab score is: %s. I'm a %s!",
+		CHAT_MESSAGE_NEW_TITLE = "You have attained a new crab title. You are now |cAAAAFF%s|r!",
+		CHAT_MESSAGE_NEXT_TITLE = "Crab score needed for next title: |cAAAAFF%s|r",
 	},
 	["de"] = {
-		MUDCRAB_TRACKER_LABEL = "Krabbenscore: %s",
-		CHAT_MESSAGE_MUDCRAB_KILLED = "Du hast %s getötet. Krabbenscore: %s",
-		CHAT_MESSAGE_BOSS_KILLED = "Endlich. Die Ufer sind frei von diesen Schrecken, die ihr ... %s nennt. Du hast %s Punkte erhalten! Krabbenscore: %s",
+		MUDCRAB_TRACKER_LABEL = "Krabbenscore: |cAAAAFF%s|r",
+		MUDCRAB_TRACKER_TITLE = "Titel: |cAAAAFF%s|r",
+		CHAT_MESSAGE_MUDCRAB_KILLED = "Du hast |cAAAAFF%s|r getötet. Krabbenscore: |cAAAAFF%s|r",
+		CHAT_MESSAGE_BOSS_KILLED = "Endlich. Die Ufer sind frei von diesen Schrecken, die ihr ... |cAAAAFF%s|r nennt. Du hast |cAAAAFF%s|r Punkte erhalten! Krabbenscore: |cAAAAFF%s|r",
 		CHAT_MESSAGE_TRACKER_TOGGLE_ON = "Mudcrab Tracker ist jetzt sichtbar.",
 		CHAT_MESSAGE_TRACKER_TOGGLE_OFF = "Mudcrab Tracker ist jetzt versteckt.",
-		CHAT_MESSAGE_CRABSTAT = "Krabbenscore: %s",
-		CHAT_MESSAGE_MULTIPLE_CRABS_KILLED = "Du hast %s Krabben getötet, gut gemacht! Krabbenscore: %s",
-		CHAT_MESSAGE_CRABS_TO_CHAT = "[MudcrabTracker] Mein Krabbenscore ist: %s",
+		CHAT_MESSAGE_CRABSTAT = "Krabbenscore: |cAAAAFF%s|r. Du bist ein |cAAAAFF%s|r!",
+		CHAT_MESSAGE_MULTIPLE_CRABS_KILLED = "Du hast |cAAAAFF%s|r Krabben getötet, gut gemacht! Krabbenscore: |cAAAAFF%s|r",
+		CHAT_MESSAGE_CRABS_TO_CHAT = "[MudcrabTracker] Mein Krabbenscore ist: %s. Ich bin ein %s!",
+		CHAT_MESSAGE_NEW_TITLE = "Du hast einen neuen Krabbentitel erreicht. Du bist jetzt |cAAAAFF%s|r!",
+		CHAT_MESSAGE_NEXT_TITLE = "Krabbenscore für den nächsten Titel benötigt: |cAAAAFF%s|r",
 	},
 	["ru"] = {
-		MUDCRAB_TRACKER_LABEL = "Очки крабов: %s",
-		CHAT_MESSAGE_MUDCRAB_KILLED = "%s убит. Очки крабов: %s",
-		CHAT_MESSAGE_BOSS_KILLED = "Наконец этот берег свободен от напасти, имя которой... %s. Вы заработали %s очков! Очки крабов: %s",
+		MUDCRAB_TRACKER_LABEL = "Очки крабов: |cAAAAFF%s|r",
+		MUDCRAB_TRACKER_TITLE = "Звание: |cAAAAFF%s|r",
+		CHAT_MESSAGE_MUDCRAB_KILLED = "|cAAAAFF%s|r убит. Очки крабов: |cAAAAFF%s|r",
+		CHAT_MESSAGE_BOSS_KILLED = "Наконец этот берег свободен от напасти, имя которой... |cAAAAFF%s|r. Вы заработали |cAAAAFF%s|r очков! Очки крабов: |cAAAAFF%s|r",
 		CHAT_MESSAGE_TRACKER_TOGGLE_ON = "Трекер грязекрабов теперь виден.",
 		CHAT_MESSAGE_TRACKER_TOGGLE_OFF = "Трекер грязекрабов теперь скрыт.",
-		CHAT_MESSAGE_CRABSTAT = "Очки крабов: %s",
-		CHAT_MESSAGE_MULTIPLE_CRABS_KILLED = "Вы убили %s крабов, хорошая работа! Очки крабов: %s",
-		CHAT_MESSAGE_CRABS_TO_CHAT = "[MudcrabTracker] Мои очки крабов: %s",
+		CHAT_MESSAGE_CRABSTAT = "Очки крабов: |cAAAAFF%s|r. Вы |cAAAAFF%s|r!",
+		CHAT_MESSAGE_MULTIPLE_CRABS_KILLED = "Вы убили |cAAAAFF%s|r крабов, хорошая работа! Очки крабов: |cAAAAFF%s|r",
+		CHAT_MESSAGE_CRABS_TO_CHAT = "[MudcrabTracker] Мои очки крабов: %s. Я %s!",
+		CHAT_MESSAGE_NEW_TITLE = "Вы получили новое крабозвание. Вы теперь |cAAAAFF%s|r!",
+		CHAT_MESSAGE_NEXT_TITLE = "Для следующего крабозвания нужно набрать |cAAAAFF%s|r очков крабов.",
 	},
 }
 local L = localizedStrings[clientLang] or localizedStrings["en"]
@@ -90,11 +143,36 @@ local crabNames = {
 }
 local crabsOfClientLang = crabNames[clientLang]
 if crabsOfClientLang == nil then
+	-- exit if language is unsupported
 	return
 end
 
 local function print(msg)
 	CHAT_SYSTEM:AddMessage(msg)
+end
+
+local function getPlayerTitleForScore(score)
+	local title = ""
+	for _, t in ipairs(titlesOfClientLang) do
+		if score >= t.threshold then
+			title = t.title
+		else
+			break
+		end
+	end
+	return title
+end
+
+local function getRequiredScoreForNextTitle(currentScore)
+	local requiredScore = nil
+	for _, t in ipairs(titlesOfClientLang) do
+		if t.threshold > currentScore then
+			if requiredScore == nil or t.threshold < requiredScore then
+				requiredScore = t.threshold
+			end
+		end
+	end
+	return requiredScore
 end
 
 local function updateLabel()
@@ -103,6 +181,9 @@ local function updateLabel()
 	end
 
 	MudcrabTrackerIndicatorLabel:SetText(string.format(L.MUDCRAB_TRACKER_LABEL, crabs_db.counter))
+	MudcrabTrackerIndicatorTitleLabel:SetText(
+		string.format(L.MUDCRAB_TRACKER_TITLE, getPlayerTitleForScore(crabs_db.counter))
+	)
 end
 
 local function updateCrabKillstat(targetName)
@@ -120,6 +201,7 @@ local function onCrabKilled(targetName, score)
 
 	EVENT_MANAGER:RegisterForUpdate(CRABS_ADDON_NAME .. "CrabKillDebounce", 5000, function()
 		EVENT_MANAGER:UnregisterForUpdate(CRABS_ADDON_NAME .. "CrabKillDebounce")
+		local isFirstKill = crabs_db.counter == 0
 		crabs_db.counter = crabs_db.counter + cumulativeScore
 		if cumulativeCrabs == 1 then
 			if cumulativeScore == 1 then
@@ -137,6 +219,15 @@ local function onCrabKilled(targetName, score)
 					tostring(crabs_db.counter)
 				)
 			)
+		end
+		local newPlayerTitle = getPlayerTitleForScore(crabs_db.counter)
+		if newPlayerTitle ~= lastPlayerTitle or isFirstKill then
+			lastPlayerTitle = newPlayerTitle
+			print(string.format(L.CHAT_MESSAGE_NEW_TITLE, newPlayerTitle))
+			local nextTitleScore = getRequiredScoreForNextTitle(crabs_db.counter)
+			if nextTitleScore ~= nil and nextTitleScore > crabs_db.counter then
+				print(string.format(L.CHAT_MESSAGE_NEXT_TITLE, tostring(nextTitleScore)))
+			end
 		end
 		cumulativeScore = 0
 		cumulativeCrabs = 0
@@ -204,8 +295,18 @@ local function toggleIndicatorOnSceneChange(currentScene)
 end
 
 local function crabStatToChat()
-	local statString = string.format(L.CHAT_MESSAGE_CRABS_TO_CHAT, tostring(crabs_db.counter))
+	local statString = string.format(
+		L.CHAT_MESSAGE_CRABS_TO_CHAT,
+		tostring(crabs_db.counter),
+		getPlayerTitleForScore(crabs_db.counter)
+	)
 	CHAT_SYSTEM.textEntry.editControl:InsertText(statString)
+end
+
+local function resetCrabScore()
+	crabs_db.counter = 0
+	crabs_db.crabKillstat = {}
+	updateLabel()
 end
 
 local function doCrabsDbMigrations(db)
@@ -244,6 +345,7 @@ local function onAddOnLoaded(_, addonName)
 
 	crabs_db = ZO_SavedVars:NewAccountWide("MudcrabTracker_db", CRABS_DB_VERSION, nil, defaults)
 	doCrabsDbMigrations(crabs_db)
+	lastPlayerTitle = getPlayerTitleForScore(crabs_db.counter)
 
 	updateLabel()
 	restoreIndicatorPosition()
@@ -279,10 +381,18 @@ local function onAddOnLoaded(_, addonName)
 
 	-- Slash Commands
 	SLASH_COMMANDS["/crabs"] = function()
-		print(string.format(L.CHAT_MESSAGE_CRABSTAT, tostring(crabs_db.counter)))
+		if not crabs_db then
+			return
+		end
+		print(
+			string.format(L.CHAT_MESSAGE_CRABSTAT, tostring(crabs_db.counter), getPlayerTitleForScore(crabs_db.counter))
+		)
 	end
 
 	SLASH_COMMANDS["/togglecrabs"] = function()
+		if not crabs_db then
+			return
+		end
 		if crabs_db.counterEnabled then
 			MudcrabTrackerIndicator:SetHidden(true)
 			crabs_db.counterEnabled = false
@@ -297,6 +407,10 @@ local function onAddOnLoaded(_, addonName)
 
 	SLASH_COMMANDS["/crabstochat"] = function()
 		crabStatToChat()
+	end
+
+	SLASH_COMMANDS["/resetcrabs"] = function()
+		resetCrabScore()
 	end
 end
 
